6d82ad15b739bf66c68d5772bc0c5465813e99a1
Curro Perez Bernal <currix@gmail.com>
2015-11-25 19:58:05 +0100

    new test vectorize

--- src/test_vectorize_Double.f90
+++ src/test_vectorize_Double.f90
@@ -2,18 +2,20 @@ PROGRAM VECT_TEST
   IMPLICIT NONE
   INTEGER, PARAMETER :: DP = KIND(1.0D0)
   !
-  REAL(KIND=DP) :: L(1024)
+  REAL(KIND=DP) :: L(0:1024)
   INTEGER :: I
   
-  DO I = 1, 1024
-     L(I) = REAL(I,DP)
+  DO I = 1, 10240
+     L(MOD(I,1024)) = REAL(MOD(I,1024),DP)
   END DO
   
-  DO I = 1, 1024
-     L(I)=L(I) + 1.0_DP
+  DO I = 1, 10240
+     L(MOD(I,1024))=L(MOD(I,1024)) + 1.0_DP
   END DO
 
-  L=L + 2.0_DP
+  DO I = 1, 10240
+     L=L + 2.0_DP
+  END DO
   
   PRINT *,  L(1)*L(1024)
   
--- src/test_vectorize_Single.f90
+++ src/test_vectorize_Single.f90
@@ -2,19 +2,20 @@ PROGRAM VECT_TEST
   IMPLICIT NONE
   INTEGER, PARAMETER :: SP = KIND(1.0)
   !
-  REAL(KIND=SP) :: L(1024)
+  REAL(KIND=SP) :: L(0:1024)
   INTEGER :: I
   
-  DO I = 1, 1024
-     L(I) = REAL(I,SP)
+  DO I = 1, 10240
+     L(MOD(I,1024)) = REAL(MOD(I,1024),SP)
   END DO
   
-  DO I = 1, 1024
-     L(I)=L(I) + 1.0_SP
+  DO I = 1, 10240
+     L(MOD(I,1024))=L(MOD(I,1024)) + 1.0_SP
   END DO
 
-  L=L + 2.0_SP
-  
+  DO I = 1, 10240
+     L=L + 2.0_SP
+  END DO  
   PRINT *, L(1)*L(1024)
   
 END PROGRAM VECT_TEST
